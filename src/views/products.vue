68a1748c79fc6f947affec7e0afce0196cb900ae
AshwinJacobs <ashwin.dean21@gmail.com>
2022-09-07 16:23:10 +0200

    loader

--- src/views/products.vue
+++ src/views/products.vue
@@ -5,12 +5,15 @@
     v-model="search"
     placeholder="Search by category"
   />
-  <div class="row">
+  <div v-if="products" class="row">
     <productCard
       v-for="product of filteredproducts"
       :key="product.product_id"
       :product="product"
     />
+  </div>
+  <div v-else>
+    <div class="spinner"></div>
   </div>
 </template>
 <script>
@@ -54,5 +57,42 @@ export default {
   font-size: 20px;
   background: transparent;
   color: black;
+}
+
+/* From uiverse.io by @EmmaxPlay */
+.spinner {
+  position: relative;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+}
+
+.spinner::before,
+.spinner:after {
+  content: "";
+  position: absolute;
+  border-radius: inherit;
+}
+
+.spinner:before {
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(0deg, #ff00cc 0%, #333399 100%);
+  animation: spin8932 0.5s infinite linear;
+}
+
+.spinner:after {
+  width: 85%;
+  height: 85%;
+  background-color: #212121;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+@keyframes spin8932 {
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
